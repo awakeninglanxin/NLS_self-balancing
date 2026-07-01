09babccbd80ba157bc206294313c2e412843c87a
awakeninglanxin <awakeninglanxin@users.noreply.github.com>
2026-07-01 19:46:34 +0800

    v6.0 APK rebuild

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nls.selfbalancing"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "4.0-yinyang"
+        versionCode = 6
+        versionName = "6.0-koldas"
     }
 
     buildTypes {
